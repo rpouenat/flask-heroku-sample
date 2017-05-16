a392ba413a0e20fdb3b51da35412bd0ca164fba3
guessWhoamI <guessWhoamI@GuestWhoamI.local>
2017-05-16 13:21:55 +0200

    AuthUser

--- app.py
+++ app.py
@@ -1,29 +1,38 @@
 import os
+import requests
+from bs4 import BeautifulSoup
 
-from flask import Flask, render_template, request, redirect, url_for
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask import jsonify, Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-
-DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////tmp/flask_app.db')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100))
-  email = db.Column(db.String(100))
-
-  def __init__(self, name, email):
-    self.name = name
-    self.email = email
 
 
 @app.route('/', methods=['GET'])
 def index():
-  return render_template('index.html', users=User.query.all())
+  return render_template('index.html')
+
+@app.route('/authUser', methods=['POST'])
+def authUser():
+  datas = request.get_json()
+  var_login = datas.get('var_login','')
+  password = datas.get('password','')
+
+  # Récupération du paramètre formulaire_action_args
+  url = "https://www.root-me.org/?page=login&lang=fr&ajax=1"
+  s = requests.Session()
+  r = s.get(url)
+  soup = BeautifulSoup(r.text, 'html.parser')
+  formulaire_action_args = soup.find('input', {'name': 'formulaire_action_args'}).get('value')
+  print(formulaire_action_args)
+
+  # Post de la requête 
+  post_data = {'var_ajax':'form', 'page':'login','lang':'fr','ajax':1,'formulaire_action':'login','formulaire_action_args':formulaire_action_args,'var_login':var_login,'password':password}
+  post_response = requests.post(url=url, data=post_data)
+  print(post_response.text)
+  print(post_response.cookies.get('spip_session'))
+
+
+  return jsonify(spip_session=post_response.cookies.get('spip_session')),200
 
 
 @app.route('/user', methods=['POST'])
@@ -33,7 +42,7 @@ def user():
   db.session.commit()
   return redirect(url_for('index'))
 
+
 if __name__ == '__main__':
-  db.create_all()
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port, debug=True)
